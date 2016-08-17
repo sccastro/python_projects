771c699a264cf450c1cd7c25ee496c0ff0f1e4b5
Spencer Castro <cac200222@gmail.com>
2016-08-17 08:20:21 -0600

    Added data for parsing to python_projects. Git is trying to make lba_model2 its own repository.

--- subid_cond.py
+++ subid_cond.py
@@ -1,15 +1,16 @@
 import glob
 import pandas as pd
 import numpy as np
+import csv
 
 path ='C:/Users/cac20/PycharmProjects/python_projects/lba_model2/'
 allFiles = glob.glob(path + "*.txt")
 
-print len(allFiles[0])
+
 # Lists we'll need later as well as pandas data frames
 # filenameframe = pd.DataFrame()
 frame = pd.DataFrame()
-list_ = []
+drt_list = []
 # conds = []
 # rename = []
 # subids = ['001', '002', '003', '004', '005', '006', '007', '008', '009', '010',
@@ -20,31 +21,50 @@ list_ = []
 #     cond = str(filename[4:6])
 #     conds.append(cond)
 #
-#
 # i = 1
 
+for change in allFiles:
+    with open(change, 'r') as f:
+        change = change[58:]
+        subid = str(change[13:16])
+        cond = str(change[16])
+        data = f.readlines()
+        for line in data:
+            line = line.split()
+            line.insert(0, subid + cond)
+            for spot in line:
+                if spot[0:3] == 'DRT':
+                    drt_list.append(line)
+
+print drt_list[0:2]
 
 
-for filename in allFiles:
-    filename = filename[58:]
-    subid = str(filename[13:16])
-    cond = str(filename[16])
+
+with open("output.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(drt_list)
+
+    # in_txt = np.genfromtxt(change, delimiter=',', dtype=str)
+    # print in_txt
+    # out_csv = csv.writer(open(change, 'wb'))
+    #
+    # out_csv.writerows(in_txt)
 
 
-    df = pd.read_csv(filename,index_col=None, header=0)
-    mapping = {'correct': 1, 'Wrong!!': 0, 'miss': -1}
-    df = df.replace({'correct': mapping, 'wrong': mapping, 'miss': mapping })
 
-    if filename[:3] == subid:
-        if filename[4:6] == 'Co':
-            df['subid'] = subid
-            df['condition'] = '1'
-        elif filename[4:6] == 'ST':
-            df['subid'] = subid
-            df['condition'] = '2'
-        else:
-            df['subid'] = subid
-            df['condition'] = '3'
+
+
+    # df = pd.read_csv(filename,index_col=None, header=0)
+    # mapping = {'correct': 1, 'wrong': 0, 'miss': -1}
+    # df = df.replace({'correct': mapping, 'wrong': mapping, 'miss': mapping })
+    #
+    # if filename[13:16] == subid:
+    #     if filename[16] == 'a':
+    #         df['subid'] = subid
+    #         df['condition'] = '1'
+    #     else:
+    #         df['subid'] = subid
+    #         df['condition'] = '2'
 
 
     # df = pd.concat([pd.Series(i, name = "subid"),pd.Series(conds[i], name = "condition"), df], axis = 1)
@@ -52,10 +72,9 @@ for filename in allFiles:
 
     # i = i + 1
 
-    list_.append(df)
+    # list_.append(df)
 
 
-frame = pd.concat(list_)
 
-frame.to_csv('complete/complete_data.csv')
+# frame.to_csv('complete/complete_data.csv')
 
